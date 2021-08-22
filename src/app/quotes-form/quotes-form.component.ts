1b5c4ee34baa46904e359ac5e2eb0e5b2a866396
Anthony64M <anthony.mutuku@students.moringaschool.com>
2021-08-22 14:13:36 +0300

    Modified the name of files

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Quote } from '../quote'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes-form',
@@ -7,20 +7,16 @@ import { Quote } from '../quote'
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+  quoted = new Quote(0,"","","","",new Date(),0,0);
+  @Output() add = new EventEmitter<Quote>();
 
-  newQuote = new Quote(0,"", "","",0,0, new Date());
-  @Output() addQuote = new EventEmitter<Quote>();
-
-  submitQuote() {
-    this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote(0,"", "","",0,0,  new Date());
-
+  addQuote(){
+this.add.emit(this.quoted);
+this.quoted = new Quote(0,"","","","",new Date(),0,0);
   }
-
-
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
