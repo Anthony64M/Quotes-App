087cb677033e2b490dc1a817488e9475366d4c5f
Anthony64M <anthony.mutuku@students.moringaschool.com>
2021-08-22 13:38:13 +0300

    The side form is added

---
+++ src/app/quotes-form/quotes-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html',
+  styleUrls: ['./quotes-form.component.css']
+})
+export class QuotesFormComponent implements OnInit {
+
+  newQuote = new Quote(0,"", "","",0,0, new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0,"", "","",0,0,  new Date());
+
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
